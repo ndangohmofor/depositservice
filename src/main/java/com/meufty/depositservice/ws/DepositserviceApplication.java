720dcf218168399bda772f337ccc2dc7a61af8a8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-28 06:35:12 -0400

    initial commit

---
+++ src/main/java/com/meufty/depositservice/ws/DepositserviceApplication.java
@@ -0,0 +1,13 @@
+package com.meufty.depositservice.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositserviceApplication.class, args);
+    }
+
+}
