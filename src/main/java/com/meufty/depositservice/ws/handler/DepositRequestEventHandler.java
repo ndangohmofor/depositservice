2f675644c4cd1c96d5b0d2347243d17ae50ab2b2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-29 07:09:13 -0400

    implementation for depositrequest event handler

--- src/main/java/com/meufty/depositservice/ws/handler/DepositRequestEventHandler.java
+++ src/main/java/com/meufty/depositservice/ws/handler/DepositRequestEventHandler.java
@@ -1,2 +1,22 @@
-package com.meufty.depositservice.ws.handler;public class DepositRequestEventHandler {
+package com.meufty.depositservice.ws.handler;
+
+import com.meufty.ws.core.events.DepositRequestEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = "deposit-money-topic", containerFactory = "kafkaListenerContainerFactory")
+public class DepositRequestEventHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @KafkaHandler
+    public void handle(@Payload DepositRequestEvent depositRequestEvent) {
+        LOGGER.info("Received a new deposit event: {} ", depositRequestEvent.getAmount());
+    }
+
 }
